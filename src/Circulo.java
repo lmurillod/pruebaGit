b40ac9b84d8636f37ca08dc508dcf05cd30e2eeb
Camon <neen1967123@hotmail.com>
2022-08-17 10:12:43 -0300

    4 coomit

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,5 +1,17 @@
 
 public class Circulo {
+	
+	public void x() {}
 	private Double radio;
 
+	public Double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(Double radio) {
+		this.radio = radio;
+	}
+	
+	
+	
 }
